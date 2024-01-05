11567dff961673d29cbc94b884bb9b3345eefa91
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2024-01-05 17:33:54 +0100

    139

--- firebase-logic.js
+++ firebase-logic.js
@@ -4,24 +4,26 @@
 // CODE STARTS HERE
 //------------------------------
 
-import { getFirestore, collection, doc, setDoc, updateDoc, increment } from "firebase/firestore"; 
+import { getFirestore, collection, doc, setDoc, increment } from "firebase/firestore"; 
 
-// Initialize Firestore
 const db = getFirestore();
 
-// Function to handle Like
 async function handleLike(number) {
     const numberRef = doc(db, "numbers", String(number));
-    await setDoc(numberRef, { likes: increment(1) }, { merge: true });
+    await setDoc(numberRef, { 
+        likes: increment(1),
+        score: increment(1)  // Increment score by 1 for a like
+    }, { merge: true });
 }
 
-// Function to handle Dislike
 async function handleDislike(number) {
     const numberRef = doc(db, "numbers", String(number));
-    await setDoc(numberRef, { dislikes: increment(1) }, { merge: true });
+    await setDoc(numberRef, { 
+        dislikes: increment(1),
+        score: increment(-1)  // Decrement score by 1 for a dislike
+    }, { merge: true });
 }
 
-// Export the functions to be used in other scripts
 export { handleLike, handleDislike };
 
 //------------------------------
