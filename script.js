16d2a92815eed4b672b07f77378c2138fa3d66a5
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2024-01-05 17:54:16 +0100

    143

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ import { handleLike, handleDislike } from './firebase-logic.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const numberContainer = document.getElementById('numberContainer');
-    const likeButton = document.getElementById('likeButton');
-    const dislikeButton = document.getElementById('dislikeButton');
 
     function generateRandomNumber() {
         return Math.floor(Math.random() * 1000);
@@ -19,26 +17,33 @@ document.addEventListener('DOMContentLoaded', () => {
         numberContainer.textContent = generateRandomNumber();
     }
 
-    likeButton.addEventListener('click', () => {
-        const number = numberContainer.textContent;
-        handleLike(number); // Update like count in Firestore
-        displayNumber();
-    });
+    // Check if like and dislike buttons exist
+    const likeButton = document.getElementById('likeButton');
+    const dislikeButton = document.getElementById('dislikeButton');
 
-    dislikeButton.addEventListener('click', () => {
-        const number = numberContainer.textContent;
-        handleDislike(number); // Update dislike count in Firestore
-        displayNumber();
-    });
-
-    document.addEventListener('keydown', (event) => {
-        if (event.code === 'ArrowRight') {
+    if (likeButton && dislikeButton) {
+        likeButton.addEventListener('click', () => {
             const number = numberContainer.textContent;
-            handleLike(number); // Update like count in Firestore
+            handleLike(number);
             displayNumber();
-        } else if (event.code === 'ArrowLeft') {
+        });
+
+        dislikeButton.addEventListener('click', () => {
             const number = numberContainer.textContent;
-            handleDislike(number); // Update dislike count in Firestore
+            handleDislike(number);
+            displayNumber();
+        });
+    }
+
+    // Keyboard event listener
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'ArrowRight' && likeButton) {
+            const number = numberContainer.textContent;
+            handleLike(number);
+            displayNumber();
+        } else if (event.code === 'ArrowLeft' && dislikeButton) {
+            const number = numberContainer.textContent;
+            handleDislike(number);
             displayNumber();
         }
     });
