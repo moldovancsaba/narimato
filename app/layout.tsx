ad20cb602301aba8aa2262700d7d1f2c0a46c961
Frame It Now Developer <moldovancsaba@gmail.com>
2025-08-28 06:39:48 +0200

    Fix syntax error in layout.tsx - build tested and successful

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
-}}) {
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
